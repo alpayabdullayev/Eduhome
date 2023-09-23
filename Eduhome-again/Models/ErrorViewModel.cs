b0b94f36dd5de2f8df4de444baa64d058df40846
alpayabdullayev <alpayabdullayev0@gmail.com>
2023-09-23 11:17:10 +0400

    Add project files.

---
+++ Eduhome-again/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Eduhome_again.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
