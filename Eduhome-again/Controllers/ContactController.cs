130cc6a690808067bc4682694e2e52252e52b29d
alpayabdullayev <alpayabdullayev0@gmail.com>
2023-09-23 11:35:29 +0400

    Front to Back

---
+++ Eduhome-again/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eduhome_again.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Eduhome-again/Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eduhome_again.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
