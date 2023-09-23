8b6ecddf9506481f1ac94a86c11d0d2104cf21bd
alpayabdullayev <alpayabdullayev0@gmail.com>
2023-09-23 17:09:39 +0400

    added Admin panel

---
+++ Eduhome-again/Areas/Admin/Controllers/ServicesController.cs
@@ -0,0 +1,26 @@
+﻿using Eduhome_again.DAL;
+using Eduhome_again.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Eduhome_again.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ServicesController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public ServicesController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult>  Index()
+        {
+            List<Service> services = await _db.Services.ToListAsync();
+            return View(services);
+        }
+    }
+}
--- Eduhome-again/Startup.cs
+++ Eduhome-again/Startup.cs
@@ -57,6 +57,10 @@ namespace Eduhome_again
 
             app.UseEndpoints(endpoints =>
             {
+                 endpoints.MapControllerRoute(
+                     name: "areas",
+                     pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+                );
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
